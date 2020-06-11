b88b5d991398c489e4f13541e55b4ec240bde123
Paulo Sales <salespaulo08gmail.com>
2020-06-11 14:44:54 -0300

    Incluindo headers

--- app/schema/index.js
+++ app/schema/index.js
@@ -2,13 +2,12 @@
 
 const { joi } = require('fvi-node-utils/app/objects')
 
-const schema = joi
-    .object({
-        url: joi.string().required(),
-        timeout: joi.number().integer().default(0).positive(),
-        delay: joi.number().integer().default(0).positive(),
-        mock: joi.boolean().default(false),
-    })
-    .options({ stripUnknown: true })
+const schema = joi.object({
+    url: joi.string().required(),
+    timeout: joi.number().integer().default(0).positive(),
+    delay: joi.number().integer().default(0).positive(),
+    headers: joi.object().options({ stripUnknown: true }),
+    mock: joi.boolean().default(false),
+})
 
 module.exports = schema
