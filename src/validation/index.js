fca0151861821cf6b4ce8de15b99d426e3afdfd0
salespaulo <salespaulo08@gmail.com>
2021-09-22 17:47:27 -0300

    Fix deps version and improve unit tests

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,20 +1,19 @@
 'use strict'
 
 const {
-    Validator,
-    Required,
-    isString,
-    isInteger,
-    isMin,
-    isObject,
-    isBoolean,
-    isNotEmpty,
+  Validator,
+  Required,
+  isString,
+  isInteger,
+  isMin,
+  isObject,
+  isBoolean,
+  isNotEmpty,
 } = require('fvi-js-validator')
 
 const url = [isString(), isNotEmpty()]
 const timeout = [isInteger(), isMin(1)]
-const delay = [isInteger(), isMin(1)]
 const headers = [isObject()]
 const mock = [isBoolean()]
 
-module.exports = Validator({ url: Required(url), timeout, delay, headers, mock })
+module.exports = Validator({ url: Required(url), timeout, headers, mock })
